3854b783f903e5e7b2b402229207ea88258d6975
limina1 <liminal@duck.com>
2025-01-04 19:22:55 -0500

    update consts file

--- src/lib/consts.ts
+++ src/lib/consts.ts
@@ -1,7 +1,12 @@
+import { writable } from "svelte/store";
 export const wikiKind = 30818;
 export const indexKind = 30040;
-export const zettelKinds = [ 30041 ];
-export const standardRelays = [ "wss://thecitadel.nostr1.com", "wss://relay.noswhere.com" ];
+export const zettelKinds = [30041];
+export const standardRelays = [
+  "wss://thecitadel.nostr1.com",
+  "wss://relay.noswhere.com",
+];
+export const networkFetchLimit = writable(5);
 
 export enum FeedType {
   Relays,
