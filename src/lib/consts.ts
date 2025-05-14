34800fbc52090c34a87b297798742b8909c0383f
limina1 <liminal@duck.com>
2025-05-14 12:38:16 -0400

    Update color pallete for medschlr

--- src/lib/consts.ts
+++ src/lib/consts.ts
@@ -1,13 +1,13 @@
 export const wikiKind = 30818;
 export const indexKind = 30040;
-export const zettelKinds = [ 30041, 30818 ];
-export const standardRelays = [ 'wss://thecitadel.nostr1.com', 'wss://theforest.nostr1.com' ];
-export const bootstrapRelays = [ 'wss://purplepag.es', 'wss://relay.noswhere.com' ];
+export const zettelKinds = [30041, 30818];
+export const standardRelays = ["wss://medschlr.nostr1.com"];
+export const bootstrapRelays = ["wss://medschlr.nostr1.com"];
 
 export enum FeedType {
-  StandardRelays = 'standard',
-  UserRelays = 'user',
+  StandardRelays = "standard",
+  UserRelays = "user",
 }
 
-export const loginStorageKey = 'alexandria/login/pubkey';
-export const feedTypeStorageKey = 'alexandria/feed/type';
+export const loginStorageKey = "alexandria/login/pubkey";
+export const feedTypeStorageKey = "alexandria/feed/type";
